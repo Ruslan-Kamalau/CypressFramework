3025703a0b69f0ddbd43a5013517aeb1de8f6374
Ruslan-Kamalau <ruslan.kamalau@gmail.com>
2025-04-08 16:17:36 +0200

    updated Address Book Page

--- cypress/support/step_definitions/pages/addressBookPage.ts
+++ cypress/support/step_definitions/pages/addressBookPage.ts
@@ -1,18 +1,31 @@
 export class AddressBookPage {
-    
-    private accountListButton = '#nav-link-accountList';
-    private addressBookLink = 'a[href*="addressselect"]';
-    private addressBoxes = '.a-box';
   
-    navigateToAddressBook() {
-      cy.get(this.accountListButton).click();
-      cy.get(this.addressBookLink).click();
-    }
+  private accountListButton = '#nav-link-accountList';
+  private addressBookLink = 'a[href*="addressselect"]';
+  private addressBoxes = '.a-box';
+  private addressPageHeader = 'h1'; 
   
-    checkSavedAddresses() {
-      cy.get(this.addressBoxes).should('exist').and('be.visible');
-      cy.get(this.addressBoxes).each((address) => {
-        cy.wrap(address).should('not.be.empty');
-      });
-    }
+  navigateToAddressBook() {
+    cy.log('Navigating to the Address Book page');
+    cy.get(this.accountListButton).click();
+    cy.get(this.addressBookLink).should('be.visible').click();
+    cy.url().should('include', '/addressselect'); 
+    cy.log('Successfully navigated to Address Book');
   }
+
+  checkSavedAddresses() {
+    cy.log('Checking for saved delivery addresses');
+    cy.get(this.addressBoxes).should('exist').and('be.visible');
+    cy.get(this.addressBoxes).each((address, index) => {
+      cy.wrap(address)
+        .should('be.visible')
+        .and('not.be.empty');
+      cy.log(`Verified address box #${index + 1}`);
+    });
+  }
+
+  verifyAddressBookPageLoaded() {
+    cy.log('Verifying that the address book page has fully loaded');
+    cy.get(this.addressPageHeader).should('contain.text', 'Your Addresses');
+  }
+}
